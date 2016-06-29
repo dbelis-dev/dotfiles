9694fbf4c11daa97504a7477853bccde8b9aa320
Dimitrios Belis <demetrios@belis.net>
2016-06-29 14:13:11 +0100

    Refactored the structure, Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,32 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-GPL.txt" \
 		--exclude "LICENSE-MIT.txt" --exclude "Putty_Theme.txt" \
-        -av --no-perms . ~
+        -av --no-perms $dir/. ~
 }
+
+function selectProfile() {
+    shopt -s dotglob
+    shopt -s nullglob
+    array=(profile*/)
+    PS3="Which profile do you want applied? "
+    echo "There are ${#array[@]} profiles in the current path"; \
+        select dir in "${array[@]}"; do echo "About to apply: ${dir}"'!'; break; done
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    selectProfile
+    if [ -n "$dir" ]; then
+        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            doIt
+        fi
+    fi
+    unset selectProfile
+    shopt -u dotglob
+    shopt -u nullglob
 fi
 unset doIt
 source ~/.bash_profile
